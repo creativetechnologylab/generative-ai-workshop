f94d394418e7e601701d625a8eef80ca2128e7ae
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2025-07-25 12:27:02 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,4 +2,4 @@
 
 conda create --name gen-ai python=3.10 -y
 conda activate gen-ai
-/home/ual/miniforge3/envs/gen-ai/bin/pip install -r requirements.txt
+/home/$USER/miniforge3/envs/gen-ai/bin/pip install -r requirements.txt
