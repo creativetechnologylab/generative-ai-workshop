c7416f241edccc913e9205483f722e433c0a8f78
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2025-10-28 15:52:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,6 @@ bash ./Miniforge3-Linux-x86_64.sh -b
 /home/$USER/miniforge3/bin/conda init
 rm ./Miniforge3-Linux-x86_64.sh
 cd generative-ai-workshop
-# exec bash
 
 /home/ual/miniforge3/bin/conda create --name gen-ai python=3.10 -y
 /home/ual/miniforge3/bin/conda activate gen-ai
